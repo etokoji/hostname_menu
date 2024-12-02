66cd5c7b54be776bd40eeca4dfc981176484bdec
etokoji <etokoji@gmail.com>
2024-12-02 12:11:22 +0900

    version 0.1 単にメニューにホスト情報を表示するバージョン。

---
+++ hostname_menu/MenuView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Hello, Swift!")
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
